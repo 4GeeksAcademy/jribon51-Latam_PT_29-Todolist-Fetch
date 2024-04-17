cf0513980d94bd977307a3066f29c7d40490e820
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-04-17 05:26:31 +0000

    proyecot completo probado

--- src/js/component/Componente.js
+++ src/js/component/Componente.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 
-const Componente=()=>{
+const Componente = () => {
+    //logica
+    const getTodosURL = "https://playground.4geeks.com/todo/users/jribon51"
+    //fetch(url de la api, metodos,body, informacion es un Json)
+    //.then(codigo de estatus y el mesnaje, se convierte json a javascript)
+    //.then(informacion que nos llego de la api)
+    //.catch(si algo sale mal obteneomos informacion del error)
+    //si el metodo no se especifica en el fetch el automaticamente lo interpreta como un GET
+
+    /* fetch(getTodosURL, {
+         method: "GET"
+     })
+         .then((reponse) => {
+             console.log(reponse)
+             return reponse.json
+ 
+         })
+         .then((data) => {
+             console.log(data)
+         })
+         .catch((error) => { error })*/
+
+    const [todos, setTodos] = useState([])
     
+    useEffect(()=>{
+        fetch(getTodosURL)
+        .then(response => response.json())
+        .then(data => {
+            setTodos(data.todos)
+        })
+        .catch((error) => { error })
+    },[])
+    
+
+    return (
+        <div className="text-center">
+            <h1>Todo's</h1>
+            {todos.map((value, index) => {
+                return <h3 key={index}>{value.label}</h3>
+            })}
+        </div>
+
+    );
+
 }
 
 export default Componente;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { renderToNodeStream } from "react-dom/server";
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //import { faTrash } from '@fortawesome/free-solid-svg-icons';
@@ -7,37 +7,98 @@ import { renderToNodeStream } from "react-dom/server";
 
 //create your first component
 const Home = () => {
-	//	const [tarea, setTarea] = useState("");
+	const getTodosURL = "https://playground.4geeks.com/todo/users/jribon51"
+	const postTodosURL = "https://playground.4geeks.com/todo/todos/jribon51"
+	const eliminarURL = "https://playground.4geeks.com/todo/todos/"
+
 	const [tareas, setTareas] = useState([]);
 	const [inputValue, setInputValue] = useState('');
+
+
 
 	const handleEnterPress = (event) => {
 		if (event.key === 'Enter') {
 			handleAddTodo();
 		}
+
 	};
 
 	const handleInputChange = (event) => {
 		setInputValue(event.target.value);
+
 	};
 
 	const handleAddTodo = () => {
-		if (inputValue.trim() !== '') { //si el input limpio de espacios es distinto a string vacío
-			setTareas([...tareas,inputValue]); //lo agrega al array tareas
-			setInputValue(''); // Limpia el input
-		}
+		addTodo(inputValue);
+
 	};
 
-	const handleDeleteTodo = (index) => {
-		const newTareas = [...tareas]; // Hace una copia del array en la variable
-		newTareas.splice(index, 1); //recorta del array, según el index dado
-		setTareas(newTareas); //El array sin el elemento recortado es el nuevo valor de todos
+	const handleDeleteTodo = (idTodo) => {
+		fetch(eliminarURL + idTodo, {
+			method: "DELETE",
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(response => {
+				if (response.status == 204) {
+					getTodos();
+				}
+
+
+			})
+			.catch(error => error)
 	};
+
+
+
+
+
+
+	function getTodos() {
+
+		fetch(getTodosURL)
+			.then(response => response.json())
+			.then(data => {
+				setTareas(data.todos)
+			})
+			.catch((error) => { error })
+
+	}
+
+	function addTodo(entrada) {
+		const newTodo = {
+			"label": entrada
+		}
+		fetch(postTodosURL, {
+			method: "POST",
+			body: JSON.stringify(newTodo),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+
+		})
+			.then(response => {
+				response.json()
+				getTodos();
+			})
+			.then(data => {
+				console.log(data);
+
+
+			})
+			.catch(error => error)
+	};
+
+	useEffect(() => {
+		getTodos()
+	}, [])
+
 
 	return (
 		<div className="text-center mt-5 container">
 			<h1>Todo</h1>
-			<div className="">
+			<div>
 				{/* <label htmlFor="todoInput" className="form-label">¡Creá tu lista de Tareas!</label> */}
 				<input
 					type="text"
@@ -48,12 +109,13 @@ const Home = () => {
 					className="form-control"
 				/>
 			</div>
-			<div> 
+			<div>
 				<ul className="list-group">
-					{tareas.map((tarea, index) => (
-						<li key={index} className="text-primary fs-3 px-5 list-group-item d-flex justify-content-between align-items-center">
-							{tarea}
-							<button onClick={() => handleDeleteTodo(index)} className="btn btn-danger boton">
+
+					{tareas.map((tarea) => (
+						<li key={tarea.id} className="text-primary fs-3 px-5 list-group-item d-flex justify-content-between align-items-center">
+							{tarea.label}
+							<button onClick={() => handleDeleteTodo(tarea.id)} className="btn btn-danger boton">
 								<i class="fas fa-trash-alt"></i>
 							</button>
 						</li>
